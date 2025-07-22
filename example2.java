4afac8ab98c9002d774bd9f7cbebfb747e086666
Ishanvireddy79 <2420080071@klh.edu.in>
2025-07-22 14:05:26 +0530

    Add files via upload
    skill lab:
    1.primitive data types
    2.Reference/object data type
    3.implicit type casting explicit type casting

---
+++ example2.java
@@ -0,0 +1,11 @@
+package Primitivecapital;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greeting = "Hello world!";
+		System.out.println(greeting);
+	}
+
+}
---
+++ implicitcasting.java
@@ -0,0 +1,15 @@
+package Typecasting;
+
+public class implicitcasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intNumber = 100;
+		long longNumber;
+		//implicit casting from int to long
+		longNumber = intNumber;
+        System.out.println("integer value:"+intNumber);
+        System.out.println("Long value after implicit casting:"+longNumber);
+	}
+
+}
