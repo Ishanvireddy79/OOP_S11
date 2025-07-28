265cfab922c6efdb14241f6ab2225bccb52c9727
Ishanvi reddy <2420080071@klh.edu.in>
2025-07-28 18:59:23 +0530

    Add files via upload
    PRACTICAL LAB:
    Arrays
    Control structuress
    legacy classes
    operators
    string comparisions
    string methods

---
+++ String_comparisions.java
@@ -0,0 +1,16 @@
+package practical1;
+
+public class String_comparisions {
+	public static void main(String[]args) {
+		String str1 = "Hello";
+		String str2 = "Hello";
+		String str3 = new String("Hello");
+		String str4 = "hello";
+		System.out.println("str1 == str2:"+(str1 == str2));
+		System.out.println("str1 == str3:"+(str1 = str3));
+		System.out.println("str1.equals(str3):"+str1.equals(str3));
+		System.out.println("str1.equalsIgnoreCase(str4):"+str1.compareTo(str4));
+		System.out.println("str1.comapreTo(str4):"+str1.compareTo(str4));
+		System.out.println("str1.compareTo(str2):"+str1.compareTo(str2));
+	}
+}
