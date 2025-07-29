f32970a25c6561f08016ac01d815aa66466e520d
Ishanvi reddy <2420080071@klh.edu.in>
2025-07-29 11:13:02 +0530

    Add files via upload

---
+++ PRACTICAL1/explicitcasting.java
@@ -0,0 +1,14 @@
+package Typecasting;
+
+public class explicitcasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double decimalNumber = 9.78;
+		int wholeNumber;
+		wholeNumber = (int)decimalNumber;
+        System.out.println("Double value:"+ decimalNumber);
+        System.out.println("Integer value after casting:"+ wholeNumber);
+	}
+
+}
