78c5d106110dbad14ff1f7dc2ba9488de0e0313a
Ishanvi reddy <2420080071@klh.edu.in>
2025-07-29 11:39:20 +0530

    Add files via upload

--- SKILL1/Autoboxing.java
+++ SKILL1/Autoboxing.java
@@ -1,1 +1,14 @@
+package wrapper_classes;
 
+public class Autoboxing {
+	public class WrapperExample1
+	{  
+	public static void main(String args[])
+	{  
+	    int a=20;  
+	   Integer i=Integer.valueOf(a);
+	   Integer j=a; 
+	   System.out.println(a+" "+i+" "+j);  
+	  }
+	}  
+}
